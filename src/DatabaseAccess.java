bc39e0744773a8b6117fe2542bca9e7d15d4f303
Camilla Teodoro <camillateodoro@hotmail.com>
2023-03-19 11:48:49 -0300

    added comments

--- src/DatabaseAccess.java
+++ src/DatabaseAccess.java
@@ -16,6 +16,11 @@ public class DatabaseAccess {
         this.filepath = filePath;
     }
 
+    /**
+     * close the database file
+     * 
+     * @throws IOException
+     */
     public void close() throws IOException {
         databaseFile.close();
     }
@@ -37,10 +42,8 @@ public class DatabaseAccess {
 
             Film film = new Film();
 
-            // System.out.println("cheguei aqui.");
             databaseFile.seek(pointerPosition + 2);
             int sizeFilm = databaseFile.readInt();
-            // System.out.println(sizeFilm);
             byte[] b = new byte[sizeFilm];
             databaseFile.read(b);
             film.fromByteArray(b);
@@ -87,6 +90,8 @@ public class DatabaseAccess {
     }
 
     /**
+     * create a new record in database
+     * 
      * @throws IOException
      */
     public Boolean create(Film film) throws IOException {
@@ -118,6 +123,7 @@ public class DatabaseAccess {
     }
 
     /**
+     * update a record
      * @throws IOException
      */
     public Boolean update(Film film, Film editedFilm) throws IOException {
@@ -128,20 +134,20 @@ public class DatabaseAccess {
 
             if (editedFilmByteArray.length <= filmByteArray.length) {
                 databaseFile.seek(find(film.getShow_id()));
-                databaseFile.writeChar('$'); // sinal de registro ativo
+                databaseFile.writeChar('$'); // character that says the file is active
 
-                databaseFile.writeInt(filmByteArray.length); // mantem o size antigo
+                databaseFile.writeInt(filmByteArray.length); // keep the old size
                 databaseFile.write(editedFilmByteArray);
 
             } else {
                 databaseFile.seek(find(film.getShow_id()));
-                databaseFile.writeChar('*'); // sinal de registro desativado
+                databaseFile.writeChar('*'); // character that says the file is deleted
 
                 databaseFile.seek(databaseFile.length());
-                databaseFile.writeChar('$'); // sinal de registro ativo
+                databaseFile.writeChar('$'); // character that says the file is active
                 databaseFile.writeInt(editedFilmByteArray.length);
                 databaseFile.write(editedFilmByteArray);
-                // Volta ponteiro para cabeçalho e atualiza id
+                // returns the pointer to the header and update id
                 databaseFile.seek(0);
                 databaseFile.writeInt(film.show_id);
 
@@ -166,7 +172,7 @@ public class DatabaseAccess {
             Long pointerPosition = find(id);
 
             databaseFile.seek(pointerPosition);
-            databaseFile.writeChar('*'); // informa que o arquivo está deletado
+            databaseFile.writeChar('*'); // character that says the file is deleted
             return true;
 
         } catch (Exception e) {
@@ -175,26 +181,48 @@ public class DatabaseAccess {
         return false;
     }
 
+    /**
+     * clear database
+     * 
+     * @throws IOException
+     */
     public void clearDb() throws IOException {
         databaseFile.setLength(0);
         databaseFile.writeInt(0);
         resetPosition();
     }
 
+    /**
+     * 
+     * @return database size
+     * @throws IOException
+     */
     public Long length() throws IOException {
         return databaseFile.length();
     }
 
+    /**
+     * return position to the beginnig of file
+     */
     public void resetPosition() {
         position = (long) 4;
         isEndOfFile = false;
     }
 
+    /**
+     * Check if is end of file
+     * 
+     * @return
+     */
     public boolean isEndOfFile() {
         return isEndOfFile;
     }
 
-    // Get next record and return the film object
+    /**
+     * Get next record and return the film object
+     * 
+     * @return a Film or null
+     */
     public Film next() {
         try {
             databaseFile.seek(position);
@@ -218,8 +246,6 @@ public class DatabaseAccess {
             }
 
             Film film = new Film();
-
-            // System.out.println("cheguei aqui.");
             databaseFile.seek(pointerPosition + 2);
             int sizeFilm = databaseFile.readInt();
             // System.out.println(sizeFilm);
@@ -236,6 +262,9 @@ public class DatabaseAccess {
         return null;
     }
 
+    /**
+     * Print database
+     */
     public void print() {
         long oldPos = position;
         position = (long) 4;
@@ -249,10 +278,22 @@ public class DatabaseAccess {
         position = oldPos;
     }
 
+    /**
+     * Get position
+     * 
+     * @return position
+     * @throws IOException
+     */
     public Long getPosition() throws IOException {
         return position;
     }
 
+    /**
+     * Set position
+     * 
+     * @param pointerPosition
+     * @throws IOException
+     */
     public void setPosition(Long pointerPosition) throws IOException {
         this.position = pointerPosition;
     }
