69c056da872814567f7b775ea368650323a09e9c
Camilla Teodoro <camillateodoro@hotmail.com>
2023-05-12 16:55:16 -0300

    add comments

--- src/LZW.java
+++ src/LZW.java
@@ -12,6 +12,13 @@ public class LZW {
 
     }
 
+    /**
+     * Method to save a compacted string in a file
+     * 
+     * @param fileAsString db file as a string
+     * @param fileName     name of the destination file
+     * @throws IOException
+     */
     public void compress(String fileAsString, String fileName) throws IOException {
         long tempoInicial = System.currentTimeMillis();
 
@@ -23,10 +30,13 @@ public class LZW {
 
         }
         // System.out.println("Tamanho do dicionário inicial: " + dictionary.size());
+
         String current = "";
+        // List where the codes will be saved before writing it in file
         List<Integer> result = new ArrayList<>();
         DataOutputStream compressedFile = new DataOutputStream(new FileOutputStream(fileName));
 
+        // loop through the string to compress
         for (char c : fileAsString.toCharArray()) {
             String combined = current + c;
             if (dictionary.containsKey(combined)) {
@@ -40,15 +50,12 @@ public class LZW {
             }
         }
 
-        // System.out.println("Tamanho do dicionário após inserção: " +
-        // dictionary.size());
-
+        // saves the code of the last char/string in result
         if (!current.equals("")) {
             result.add(dictionary.get(current));
         }
 
-        // System.out.println("Tamanho do result após cada inserção: " + result.size());
-
+        // loop through result and write the codes in the file
         for (Integer code : result) {
             compressedFile.writeInt(code);
 
@@ -62,8 +69,15 @@ public class LZW {
         System.out.println("Tempo total para compactação pelo algoritmo LZW foi de " + total + " milessegundos");
     }
 
+    /**
+     * Method to unpack a file and overwrite the banco.db file
+     * 
+     * @param CompressedFileName file to unpack
+     * @param newDBFile          original db file to be overwritten
+     */
     public void unpack(String CompressedFileName, String newDBFile)
             throws IOException, NumberFormatException, ParseException {
+
         long tempoInicial = System.currentTimeMillis();
 
         // create the initial dictionary
@@ -78,6 +92,7 @@ public class LZW {
 
         List<Integer> compressed = new ArrayList<>();
 
+        // read the compressed file and populate the list with the codes
         while (compressedFile.available() > 0) {
             compressed.add(compressedFile.readInt());
         }
@@ -86,6 +101,9 @@ public class LZW {
         int code = compressed.remove(0);
         String current = dictionary.get(code);
         String unpackedFileString = current;
+
+        // read the list and search/populate the dictionary to create the unpacked
+        // string
         for (int nextCode : compressed) {
             String entry;
             if (dictionary.containsKey(nextCode)) {
