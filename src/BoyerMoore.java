e31c9077d38f16cc67534a900e32c52339058223
Camilla Teodoro <camillateodoro@hotmail.com>
2023-06-01 18:46:07 -0300

    minor changes

--- src/BoyerMoore.java
+++ src/BoyerMoore.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class BoyerMoore {
-    long tempoInicial = System.currentTimeMillis();
 
     private static int NO_OF_CHARS = 256; // Assuming an extended ASCII character set
 
@@ -20,7 +19,8 @@ public class BoyerMoore {
         // Store the rightmost occurrence of each character in the pattern, except for
         // the last character
         for (int i = 0; i < patternLength - 1; i++) {
-            badChar[pattern[i]] = i;
+            char currentChar = pattern[i];
+            badChar[currentChar] = i;
         }
     }
 
@@ -29,10 +29,10 @@ public class BoyerMoore {
      */
     private static void goodSuffixHeuristic(char[] pattern, int[] goodSuffix) {
         int patternLength = pattern.length;
-        //
         int[] suffixes = new int[patternLength];
         int lastPrefixPosition = patternLength;
 
+        // stores the lengths of the suffixes for each position in the pattern array
         for (int i = patternLength - 1; i >= 0; i--) {
             if (isPrefix(pattern, i + 1)) {
                 lastPrefixPosition = i + 1;
@@ -48,8 +48,8 @@ public class BoyerMoore {
             suffixes[suffixLength] = patternLength - 1 - i + suffixLength;
         }
 
+        // initialize the goodSuffix array with the pattern length
         for (int i = 0; i < patternLength; i++) {
-            // Initialize the goodsuffix array with the default shift value
             goodSuffix[i] = patternLength;
         }
 
@@ -107,17 +107,19 @@ public class BoyerMoore {
      * 
      * @param text
      * @param pattern
-     * @return
+     * @return a list with all occurrences
      */
     public List<Integer> boyerMooreSearch(String fileAsString, String pattern) {
+        long tempoInicial = System.currentTimeMillis();
+
         List<Integer> occurrences = new ArrayList<>();
 
         // Convert the fileAsString and pattern strings into character arrays
-        char[] textChars = fileAsString.toCharArray();
-        char[] patternChars = pattern.toCharArray();
+        char[] textArray = fileAsString.toCharArray();
+        char[] patternArray = pattern.toCharArray();
 
-        int textLength = textChars.length;
-        int patternLength = patternChars.length;
+        int textLength = textArray.length;
+        int patternLength = patternArray.length;
 
         // count the pattern shifts inside the text
         int patternShifts = 0;
@@ -126,8 +128,8 @@ public class BoyerMoore {
         int[] goodSuffix = new int[patternLength + 1];
 
         // Preprocess the bad Char and the good suffix
-        badCharHeuristic(patternChars, badChar);
-        goodSuffixHeuristic(patternChars, goodSuffix);
+        badCharHeuristic(patternArray, badChar);
+        goodSuffixHeuristic(patternArray, goodSuffix);
 
         int shift = 0;
         // Loop that continues as long as the shift is less than or equal to the
@@ -136,7 +138,7 @@ public class BoyerMoore {
             int j = patternLength - 1;
 
             // Compare the pattern with the text character by character from right to left
-            while (j >= 0 && patternChars[j] == textChars[shift + j]) {
+            while (j >= 0 && patternArray[j] == textArray[shift + j]) {
                 j--;
             }
 
@@ -146,7 +148,7 @@ public class BoyerMoore {
                 // Apply the shift based on the good suffix array
                 shift += goodSuffix[0];
             } else {
-                int badCharShift = j - badChar[textChars[shift + j]];
+                int badCharShift = j - badChar[textArray[shift + j]];
                 int goodSuffixShift = goodSuffix[j + 1];
                 // shift the pattern according to the max jump
                 shift += Math.max(badCharShift, goodSuffixShift);
@@ -160,6 +162,7 @@ public class BoyerMoore {
         System.out.println("Tempo total para busca pelo algoritmo Boyer-Moore foi de " + total + " milessegundos");
         System.out.println("Numero de movimentações do padrão pelo algoritmo Boyer-Moore foi de " + patternShifts
                 + " movimentações");
+
         return occurrences;
     }
 }
